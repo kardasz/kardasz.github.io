7f4290ae15780e8e9e10d4b4271aa237848b6e4b
Krzysztof Kardasz <krzysztof@kardasz.eu>
2022-01-07 21:04:39 +0100

    Kardasz HomePage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import zfce from './zfce.jpg';
+import zce from './zce.jpg';
+import zcpe from './zcpe.jpg';
+import sf4cda from './sf4cda.png';
+import "@fontsource/roboto";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-white space-bottom-medium">Krzysztof Kardasz</h1>
+        <span>Senior Backend Developer</span>
+          <p>
+              <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
+                  <img alt="Zend Certified PHP Engineer" src={ zcpe } />
+              </a>
+
+              <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
+                  <img alt="Zend PHP 5 Certified Engineer" src={ zce} />
+              </a>
+              <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
+                  <img alt="Zend Framework 1 Certified Engineer" src={ zfce } />
+              </a>
+              <a href="https://connect.symfony.com/profile/kardasz">
+                  <img alt="Symfony 4 Certified Developer" src={sf4cda } />
+              </a>
+          </p>
       </header>
     </div>
   );
