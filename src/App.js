86f9bca61507bd99b468be3bcefef14474622b80
Krzysztof Kardasz <krzysztof@kardasz.eu>
2022-06-04 11:08:03 +0200

    Symfony 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import zfce from './zfce.jpg';
 import zce from './zce.jpg';
 import zcpe from './zcpe.jpg';
-import sf4cda from './sf4cda.png';
+import sf4 from './sf4.png';
+import sf5 from './sf5.png';
 import "@fontsource/roboto";
 import './App.css';
 
@@ -12,6 +13,14 @@ function App() {
         <h1 className="text-white space-bottom-medium">Krzysztof Kardasz</h1>
         <span>Senior Backend Developer</span>
           <p>
+              <a href="https://connect.symfony.com/profile/kardasz">
+                  <img alt="Symfony 5 Certified Developer" src={sf5 } />
+              </a>
+
+              <a href="https://connect.symfony.com/profile/kardasz">
+                  <img alt="Symfony 4 Certified Developer" src={sf4 } />
+              </a>
+
               <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
                   <img alt="Zend Certified PHP Engineer" src={ zcpe } />
               </a>
@@ -19,11 +28,9 @@ function App() {
               <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
                   <img alt="Zend PHP 5 Certified Engineer" src={ zce} />
               </a>
+
               <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
                   <img alt="Zend Framework 1 Certified Engineer" src={ zfce } />
-              </a>
-              <a href="https://connect.symfony.com/profile/kardasz">
-                  <img alt="Symfony 4 Certified Developer" src={sf4cda } />
               </a>
           </p>
       </header>
