06274919f04f76e009cad3824c78fd345d8e8f4a
Krzysztof Kardasz <krzysztof@kardasz.eu>
2024-11-28 09:23:48 +0100

    SF7 Badge

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import zcpe from './zcpe.jpg';
 import sf4 from './sf4.png';
 import sf5 from './sf5.png';
 import sf6 from './sf6.png';
+import sf7 from './sf7.png';
 import "@fontsource/roboto";
 import './App.css';
 
@@ -15,27 +16,30 @@ function App() {
         <span>Senior Backend Developer</span>
           <p>
               <a href="https://connect.symfony.com/profile/kardasz">
-                  <img alt="Symfony 6 Certified Developer" src={sf6 } />
+                  <img alt="Symfony 6 Certified Developer" src={sf7}/>
+              </a>
+              <a href="https://connect.symfony.com/profile/kardasz">
+                  <img alt="Symfony 7 Certified Developer" src={sf6}/>
               </a>
 
               <a href="https://connect.symfony.com/profile/kardasz">
-                  <img alt="Symfony 5 Certified Developer" src={sf5 } />
+                  <img alt="Symfony 5 Certified Developer" src={sf5}/>
               </a>
 
               <a href="https://connect.symfony.com/profile/kardasz">
-                  <img alt="Symfony 4 Certified Developer" src={sf4 } />
+                  <img alt="Symfony 4 Certified Developer" src={sf4}/>
               </a>
 
               <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
-                  <img alt="Zend Certified PHP Engineer" src={ zcpe } />
+                  <img alt="Zend Certified PHP Engineer" src={zcpe}/>
               </a>
 
               <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
-                  <img alt="Zend PHP 5 Certified Engineer" src={ zce} />
+                  <img alt="Zend PHP 5 Certified Engineer" src={zce}/>
               </a>
 
               <a href="https://www.zend-zce.com/en/yellow-pages/ZEND014470">
-                  <img alt="Zend Framework 1 Certified Engineer" src={ zfce } />
+                  <img alt="Zend Framework 1 Certified Engineer" src={zfce}/>
               </a>
           </p>
       </header>
